6359b14ad169e157892a6306fa34edb674eb51d6
diana <daiana.rehan.dr@gmail.com>
2024-07-22 00:18:56 +0300

    Update Tap.cs

--- Assets/Scripts/Tap.cs
+++ Assets/Scripts/Tap.cs
@@ -57,8 +57,8 @@ public class Tap : MonoBehaviour
             }  
 
         }
-        if (transform.localEulerAngles.x > maxRotDeg / 2 && transform.localEulerAngles.x < 47) audioSource.PlayOneShot(handleSounds[0], 0.1f);
-        if (transform.localEulerAngles.x > 88 && transform.localEulerAngles.x < 90) audioSource.PlayOneShot(handleSounds[1], 0.05f);
+        if (transform.localEulerAngles.x > maxRotDeg / 2 && transform.localEulerAngles.x < 46) audioSource.PlayOneShot(handleSounds[0], 0.1f);
+        if (transform.localEulerAngles.x > 88 && transform.localEulerAngles.x < 89) audioSource.PlayOneShot(handleSounds[1], 0.05f);
 
         if (!isRotated && transform.localEulerAngles.x > minRotDeg)
         {
