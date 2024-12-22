6f5d974e3219bdea3b9818fb2fd659b30827f2a4
diana <daiana.rehan.dr@gmail.com>
2024-12-23 00:47:12 +0200

    lerping between main menu and game play scene

--- Assets/Scripts/CameraFocus.cs
+++ Assets/Scripts/CameraFocus.cs
@@ -7,7 +7,22 @@ public class CameraFocus : MonoBehaviour
     [SerializeField] private float camMoveSpeed = 5;
     [SerializeField] private float camRotSpeed = 45;
 
-    Vector3 targetPos;
-    Quaternion targetRot;
+    public static CameraFocus instance;
 
+    private void Awake()
+    {
+        transform.position = startTransform.position;
+        transform.rotation = startTransform.rotation;
+        if (instance == null)
+            instance = this;
+        
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public void StartGame()
+    {
+        transform.position= Vector3.Lerp(transform.position, machineView.position, camMoveSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, machineView.rotation, camRotSpeed);
+    }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame()
     {   
+        CameraFocus.instance.StartGame();
         SoundManager.Instance.StopMainMenuMusic();
         SoundManager.Instance.PlayGameSound();
         SoundManager.Instance.PlayRandomNoise();
